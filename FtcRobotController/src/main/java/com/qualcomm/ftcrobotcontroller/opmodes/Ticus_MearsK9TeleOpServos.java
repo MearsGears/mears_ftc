c059c853e89bd549c7e75d5034d6126c66fcc465
Ticus Brandt <ticachu907@gmail.com>
2015-11-20 12:40:29 -0900

    new new

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Ticus_MearsK9TeleOpServos.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Ticus_MearsK9TeleOpServos.java
@@ -34,221 +34,51 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Hardware;
 import com.qualcomm.robotcore.util.Range;
 
-/**
- * TeleOp Mode
- * <p>
- * Enables control of the robot via the gamepad
- */
+
+
 public class Ticus_MearsK9TeleOpServos extends OpMode {
 
-	/*
-	 * Note: the configuration of the servos is such that
-	 * as the armservo servo approaches 0, the armservo position moves up (away from the floor).
-	 * Also, as the claw servo approaches 0, the claw opens up (drops the game element).
-	 */
-	// TETRIX VALUES.
-	final static double armservo_MIN_RANGE  = 0.20;
-	final static double armservo_MAX_RANGE  = 0.90;
-	final static double CLAW_MIN_RANGE  = 0.20;
-	final static double CLAW_MAX_RANGE  = 0.7;
 
-	// position of the armservo servo.
-	double armservoPosition;
+	DcMotor leftMotor;
+	DcMotor rightMotor;
+	DcMotor leftArm;
 
-	// amount to change the armservo servo position.
-	double armservoDelta = 0.1;
-
-	// position of the claw servo
-	double clawPosition;
-
-	// amount to change the claw servo position by
-	double clawDelta = 0.1;
-
-	DcMotor motorRight;
-	DcMotor motorLeft;
-	DcMotor arm_motor;
-	DcMotor arm_extend;
-	Servo claw;
-	Servo armservo;
-
-	/**
-	 * Constructor
-	 */
-	public Ticus_MearsK9TeleOpServos() {
-
-	}
-
-	/*
-	 * Code to run when the op mode is first enabled goes here
-	 *
-	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
-	 */
 	@Override
 	public void init() {
+		//get references to the motors from hardware map
+		leftMotor = hardwareMap.dcMotor.get("left_drive");
+		rightMotor = hardwareMap.dcMotor.get("right_drive");
+		leftArm = hardwareMap.dcMotor.get("left_arm");
+
+		//reverse the left motor
+		rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
 
-		/*
-		 * Use the hardwareMap to get the dc motors and servos by name. Note
-		 * that the names of the devices must match the names used when you
-		 * configured your robot and created the configuration file.
-		 */
-
-		/*
-		 * For the demo Tetrix K9 bot we assume the following,
-		 *   There are two motors "left_drive" and "right_drive"
-		 *   "left_drive" is on the right side of the bot.
-		 *   "right_drive" is on the left side of the bot and reversed.
-		 *
-		 * We also assume that there are two servos "servo_1" and "servo_6"
-		 *    "servo_1" controls the armservo joint of the manipulator.
-		 *    "servo_6" controls the claw joint of the manipulator.
-		 */
-		arm_motor = hardwareMap.dcMotor.get("arm");
-		motorRight = hardwareMap.dcMotor.get("right_drive");
-		motorLeft = hardwareMap.dcMotor.get("left_drive");
-		arm_extend = hardwareMap.dcMotor.get("arm_drive");
-
-		motorRight.setDirection(DcMotor.Direction.REVERSE);
-
-		armservo = hardwareMap.servo.get("servo_1");
-		claw = hardwareMap.servo.get("servo_6");
-
-		// assign the starting position of the wrist and claw
-		armservoPosition = 0.2;
-		clawPosition = 0.2;
 	}
 
-	/*
-	 * This method will be called repeatedly in a loop
-	 *
-	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
-	 */
 	@Override
 	public void loop() {
+		//get values from gamepads
+		//note: pushing the stick all the way up returns -1, so we need to reverse the values
+		float leftY = -gamepad1.left_stick_y;
+		float rightY = -gamepad1.right_stick_y;
 
-		/*
-		 * Gamepad 1
-		 *
-		 * Gamepad 1 controls the motors via the left stick, and it controls the
-		 * wrist/claw via the a,b, x, y buttons
-		 */
+		//set the power of the motors with the gamepad values
+		leftMotor.setPower(leftY);
+		rightMotor.setPower(rightY);
 
-		// throttle: left_stick_y ranges from -1 to 1, where -1 is full up, and
-		// 1 is full down
-		// direction: left_stick_x ranges from -1 to 1, where -1 is full left
-		// and 1 is full right
-		float throttle = -gamepad1.left_stick_y;
-		float direction = gamepad1.left_stick_x;
-		float right = throttle - direction;
-		float left = throttle + direction;
-		float arm = gamepad2.left_stick_y;
-		float arm_drive = gamepad2.right_stick_y;
-		// clip the right/left values so that the values never exceed +/- 1
-		right = Range.clip(right, -1, 1);
-		left = Range.clip(left, -1, 1);
-		arm = Range.clip(arm, -1, 1);
-		arm_drive = Range.clip(arm_drive, -1, 1);
-		// scale the joystick value to make it easier to control
-		// the robot more precisely at slower speeds.
-		right = (float)scaleInput(right);
-		left =  (float)scaleInput(left);
-
-		// write the values to the motors
-		arm_extend.setPower(arm_drive);
-		motorRight.setPower(right);
-		motorLeft.setPower(left);
-		arm_motor.setPower(arm);
-		// update the position of the armservo.
-		if (gamepad2.right_bumper) {
-			// if the A button is pushed on gamepad1, increment the position of
-			// the armservo servo.
-			armservoPosition += armservoDelta;
-		}
-
-		if (gamepad2.left_bumper) {
-			// if the Y button is pushed on gamepad1, decrease the position of
-			// the armservo servo.
-			armservoPosition -= armservoDelta;
-		}
-
-		// update the position of the claw
-		if (gamepad2.right_bumper) {
-			clawPosition += clawDelta;
-		}
-
-		if (gamepad2.left_bumper) {
-			clawPosition -= clawDelta;
-		}
-
-		// clip the position values so that they never exceed their allowed range.
-		armservoPosition = Range.clip(armservoPosition, armservo_MIN_RANGE, armservo_MAX_RANGE);
-		clawPosition = Range.clip(clawPosition, CLAW_MIN_RANGE, CLAW_MAX_RANGE);
-
-		// write position values to the wrist and claw servo
-		armservo.setPosition(armservoPosition);
-		claw.setPosition(clawPosition);
-
-
-
-		/*
-		 * Send telemetry data back to driver station. Note that if we are using
-		 * a legacy NXT-compatible motor controller, then the getPower() method
-		 * will return a null value. The legacy NXT-compatible motor controllers
-		 * are currently write only.
-		 */
-		telemetry.addData("Text", "*** Robot Data***");
-		telemetry.addData("armservo", "armservo:  " + String.format("%.2f", armservoPosition));
-		telemetry.addData("claw", "claw:  " + String.format("%.2f", clawPosition));
-		telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", left));
-		telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", right));
-
-	}
-
-	/*
-	 * Code to run when the op mode is first disabled goes here
-	 *
-	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
-	 */
-	@Override
-	public void stop() {
-
-	}
-
-
-	/*
-	 * This method scales the joystick input so for low joystick values, the
-	 * scaled value is less than linear.  This is to make it easier to drive
-	 * the robot more precisely at slower speeds.
-	 */
-	double scaleInput(double dVal)  {
-		double[] scaleArray = { 0.0, 0.000009, 0.01, 0.02, 0.05, 0.07, 0.09, 0.10, 0.12, 0.15, 0.18, 0.21, 0.24, 0.28,
-				0.30, 0.36, 0.43, 0.47, 0.50, 0.55, 0.60, 0.66, 0.72, 0.85, 1.00, 1.00 };
-
-		// get the corresponding index for the scaleInput array.
-		int index = (int) (dVal * 16.0);
-
-		// index should be positive.
-		if (index < 0) {
-			index = -index;
-		}
-
-		// index cannot exceed size of array minus 1. 
-		if (index > 16) {
-			index = 16;
-		}
-
-		// get value from the array.
-		double dScale = 0.1;
-		if (dVal < 0) {
-			dScale = -scaleArray[index];
+		if (gamepad2.y) {
+			leftArm.setPower(0.7);
+		} else if (gamepad2.a) {
+			leftArm.setPower(-0.1);
 		} else {
-			dScale = scaleArray[index];
+			leftArm.setPower(0);
 		}
 
-		// return scaled value.
-		return dScale;
+
 	}
 
 }
