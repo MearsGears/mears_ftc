0127c49b0979da12ce3e0e483e734fc9289eb626
Ticus Brandt <ticachu907@gmail.com>
2016-01-19 11:52:55 -0900

    linda wanted one

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -63,6 +63,7 @@ public class FtcOpModeRegister implements OpModeRegister {
       manager.register("TicusOp", Ticus_MearsK9TeleOpServos.class);
       manager.register("EncoderTry", EncoderTryMearsK9TeleOpServos.class);
       manager.register("DianeEncoder", DianeEncoderTryMearsK9TeleOpServos.class);
+      manager.register("LindaOp", LindaOpMearsK9TeleOpServos.class);
 
 
    /* manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
