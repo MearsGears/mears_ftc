26f7647bba1fbb773749f4327e59399790206e73
dianemclaughlin <mclaughlin_diane@asdk12.org>
2016-01-21 15:22:15 -0900

    this one works TicusOp

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Ticus_MearsK9TeleOpServos.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Ticus_MearsK9TeleOpServos.java
@@ -43,8 +43,10 @@ public class Ticus_MearsK9TeleOpServos extends OpMode {
     final double RIGHT_OPEN_POSITION = 1.0;
     final double RIGHT_CLOSED_POSITION = 0.5;
 
-    DcMotor leftE;
-    DcMotor rightE;
+    DcMotor leftPulley;
+    DcMotor rightPulley;
+    DcMotor leftEx;
+    DcMotor rightEx;
     DcMotor leftMotor;
     DcMotor rightMotor;
     Servo leftS;
@@ -58,8 +60,11 @@ public class Ticus_MearsK9TeleOpServos extends OpMode {
         rightMotor = hardwareMap.dcMotor.get("right_motor");
         leftS = hardwareMap.servo.get("leftS");
         rightS = hardwareMap.servo.get("rightS");
-        leftE = hardwareMap.dcMotor.get("leftE");
-        rightE = hardwareMap.dcMotor.get("rightE");
+        leftEx = hardwareMap.dcMotor.get("leftE");
+        rightEx = hardwareMap.dcMotor.get("rightE");
+        leftPulley = hardwareMap.dcMotor.get("LPulley");
+        rightPulley = hardwareMap.dcMotor.get("RPulley");
+
 
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
@@ -69,19 +74,39 @@ public class Ticus_MearsK9TeleOpServos extends OpMode {
     @Override
     public void loop() {
 
+
+        float right = gamepad2.right_stick_y;           //added
+        float left = gamepad2.right_stick_y;           //added
+
+
+
+
         float leftY = -gamepad1.left_stick_y;
         float rightY = -gamepad1.right_stick_y;
 
-        float rightX = gamepad2.right_stick_y;
-        float leftX = gamepad2.right_stick_y;
+        float rightP = gamepad2.left_stick_y;
+        float leftP = gamepad2.left_stick_y;
+
+       // float rightX = gamepad2.right_stick_y;
+        //float leftX = gamepad2.right_stick_y;         took out
+
+
+        // scale the joystick value to make it easier to control
+        // the robot more precisely at slower speeds.
+        right = (float)scaleInput(right);           //added
+        left =  (float)scaleInput(left);            //added
+
+        leftP = (float)scaleInput(leftP);
+        rightP = (float)scaleInput(rightP);
 
         leftMotor.setPower(leftY);
         rightMotor.setPower(rightY);
 
-        rightE.setPower(rightX);
-        leftE.setPower(leftX);
+        rightEx.setPower(right);
+        leftEx.setPower(left);
 
-
+        leftPulley.setPower(leftP);
+        rightPulley.setPower(rightP);
 
         if (gamepad2.left_bumper) {
             leftS.setPosition(LEFT_OPEN_POSITION);
@@ -92,6 +117,47 @@ public class Ticus_MearsK9TeleOpServos extends OpMode {
             rightS.setPosition(RIGHT_CLOSED_POSITION);
         }
     }
+    @Override
+    public void stop() {
+
+    }
+
+
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = {  0.0, 0.01, 0.02, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.14, 0.15, 0.18, 0.21, 0.22, 0.25, 0.28,
+                0.30, 0.36, 0.43, 0.47, 0.50, 0.55, 0.60, 0.66, 0.72, 0.85, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+
 }
 
 
